1da482cac1814f424e4eee692a2a9fcc913faee4
Wenyi Xu <wenyixu@WenyideMacBook-Pro.local>
2018-05-26 11:04:21 -0500

    Added send_customer & send_product coroutines.

--- kafka-producer/mock_data_producer.py
+++ kafka-producer/mock_data_producer.py
@@ -10,36 +10,59 @@ class MockProducer(object):
     def __init__(self):
         self.producer = KafkaProducer(bootstrap_servers='kafka1:9092')
         self.invoiceno_pool = ['537127', '536539', '540414']
-        self.stockcode_pool = ['85123A', '84580', '21558', '22726']
-        self.customerid_pool = ['13831']
+        self.stockcode_pool = {'85123A': ['WHITE HANGING HEART T-LIGHT HOLDER', 3.24], \
+                               '84580':  ['MOUSE TOY WITH PINK T-SHIRT', 3.75], \
+                               '21558':  ['SKULL LUNCH BOX WITH CUTLERY', 2.55], \
+                               '22726':  ['ALARM CLOCK BAKELIKE GREEN', 3.75]}
+        self.customerid_pool = {'13831': 'United Kingdom', \
+                                '12605': 'Germany'}
 
     def run(self):
         loop = asyncio.get_event_loop()
         try:
-            loop.run_until_complete(asyncio.wait([self.send_invoice()]))
+            loop.run_until_complete(
+                asyncio.wait([
+                    self.send_invoice(),
+                    self.send_customer(),
+                    self.send_product()
+                ])
+            )
         finally:
             loop.close()
 
+    # invoiceno,stockcode,quantity,invoicedate,customerid
     @coroutine
     def send_invoice(self):
         while True:
             invoiceno = random.choice(self.invoiceno_pool)
-            stockcode = random.choice(self.stockcode_pool)
+            stockcode = random.choice(list(self.stockcode_pool.keys()))
             quantity = str(random.randint(1, 100))
             invoicedate = datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")
-            customerid = random.choice(self.customerid_pool)
+            customerid = random.choice(list(self.customerid_pool.keys()))
             message = ",".join([invoiceno, stockcode, quantity, invoicedate, customerid])
             self.producer.send('invoice_in', str.encode(message))
             yield from asyncio.sleep(1)
 
+    # customerid,country
     @coroutine
     def send_customer(self):
-        pass
+        while True:
+            customerid = random.choice(list(self.customerid_pool.keys()))
+            country = self.customerid_pool[customerid]
+            message = ",".join([customerid, country])
+            self.producer.send('customer_in', str.encode(message))
+            yield from asyncio.sleep(20)
 
+    # stockcode,description,unitprice
     @coroutine
     def send_product(self):
-        pass
-
+        while True:
+            stockcode = random.choice(list(self.stockcode_pool.keys()))
+            description = self.stockcode_pool[stockcode][0]
+            unitprice = str(self.stockcode_pool[stockcode][1])
+            message = ",".join([stockcode, description, unitprice])
+            self.producer.send('product_in', str.encode(message))
+            yield from asyncio.sleep(20)
 
 
 if __name__ == "__main__":
