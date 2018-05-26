439cf6859a392976916816be4ebcb054caae62e8
Wenyi Xu <wenyixu@WenyideMacBook-Pro.local>
2018-05-26 15:05:42 -0500

    Added mysql-consumer service start up.

--- bin/start.sh
+++ bin/start.sh
@@ -4,5 +4,7 @@ sudo docker-compose up -d mysql
 sleep 10s
 . ./bin/kafka-setup.sh
 sleep 10s
+sudo docker-compose up -d mysql-consumer
+sleep 10s
 sudo docker-compose build kafka-producer
 sudo docker-compose up -d kafka-producer
