6d86fd2edb796960512ffc959fd2d321ecc42a53
Wenyi Xu <wenyixu@WenyideMacBook-Pro.local>
2018-05-26 15:13:41 -0500

    Logic tests.

--- mysql-consumer/mysql_consumer.py
+++ mysql-consumer/mysql_consumer.py
@@ -1,4 +1,13 @@
 import sqlalchemy
+from kafka import KafkaConsumer
+
+# class Customer():
+#
+# 
+# class Product():
 
 if __name__ == "__main__":
-    print(sqlalchemy.__version__ )
+    # print(sqlalchemy.__version__ )
+    customer_consumer = KafkaConsumer('customer_in', bootstrap_servers='kafka1:9092')
+    for msg in customer_consumer:
+        print(msg)
