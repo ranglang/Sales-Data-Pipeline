4def2dbbac13ae4dcabec9b5f3b8b101717c72c9
Wenyi Xu <wenyixu@WenyideMacBook-Pro.local>
2018-05-26 15:50:02 -0500

    Added msg decode.

--- mysql-consumer/mysql_consumer.py
+++ mysql-consumer/mysql_consumer.py
@@ -10,7 +10,9 @@ if __name__ == "__main__":
     # for msg in customer_consumer:
     #     print(msg)
 
-    customer_consumer = KafkaConsumer('customer_in', bootstrap_servers='kafka1:9092')
+    customer_consumer = KafkaConsumer(bootstrap_servers='kafka1:9092', \
+                                      value_deserializer=lambda m: json.loads(m.decode('utf-8')))
+    customer_consumer.subscribe(['customer_in'])
 
     connection = pymysql.connect(host='mysql',
                                  user='root',
@@ -19,7 +21,7 @@ if __name__ == "__main__":
 
     with connection.cursor() as cursor:
         for msg in customer_consumer:
-            customerid, country = msg.split(',')
+            customerid, country = msg.value.split(',')
             sql = "INSERT INTO `customer` (`CUSTOMER_ID`, `COUNTRY`) VALUES (%s, %s)" % (customerid, country)
             cursor.execute(sql)
             connection.commit()
