f71d5a87e8e1a2e2e91eeaab8decfb9f78f8e2c5
Wenyi Xu <wenyixu@WenyideMacBook-Pro.local>
2018-05-26 15:43:25 -0500

    Logic tests.

--- mysql-consumer/mysql_consumer.py
+++ mysql-consumer/mysql_consumer.py
@@ -1,13 +1,27 @@
-import sqlalchemy
+import pymysql
 from kafka import KafkaConsumer
 
-# class Customer():
 #
-# 
 # class Product():
 
 if __name__ == "__main__":
     # print(sqlalchemy.__version__ )
+    # customer_consumer = KafkaConsumer('customer_in', bootstrap_servers='kafka1:9092')
+    # for msg in customer_consumer:
+    #     print(msg)
+
     customer_consumer = KafkaConsumer('customer_in', bootstrap_servers='kafka1:9092')
-    for msg in customer_consumer:
-        print(msg)
+
+    connection = pymysql.connect(host='mysql',
+                                 user='root',
+                                 password='233',
+                                 db='sales_data_pipeline')
+
+    with connection.cursor() as cursor:
+        for msg in customer_consumer:
+            customerid, country = msg.split(',')
+            sql = "INSERT INTO `customer` (`CUSTOMER_ID`, `COUNTRY`) VALUES (%s, %s)" % (customerid, country)
+            cursor.execute(sql)
+            connection.commit()
+
+    connection.close()
