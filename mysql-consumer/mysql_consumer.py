decac30bd203757b8f16f29286176508a26e1805
Wenyi Xu <wenyixu@WenyideMacBook-Pro.local>
2018-05-26 21:12:44 -0500

    Added multi-threading.

--- mysql-consumer/mysql_consumer.py
+++ mysql-consumer/mysql_consumer.py
@@ -1,32 +1,83 @@
 import pymysql
 from kafka import KafkaConsumer
 import logging
+import threading
+import time
 
-#
-# class Product():
+class customerConsumer(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+        self.stop_event = threading.Event()
+
+    def stop(self):
+        self.stop_event.set()
+
+    def run(self):
+        customer_consumer = KafkaConsumer(bootstrap_servers='kafka1:9092', \
+                                          value_deserializer=lambda m: m.decode('utf-8'))
+        customer_consumer.subscribe(['customer_in'])
+
+        connection = pymysql.connect(host='mysql',
+                                     user='root',
+                                     password='233',
+                                     db='sales_data_pipeline')
+
+        while not self.stop_event.is_set():
+
+            with connection.cursor() as cursor:
+                for msg in customer_consumer:
+                    customerid, country = msg.value.split(',')
+                    logging.debug('%s %s', customerid, country)
+                    sql = "INSERT INTO `customer` (`CUSTOMER_ID`, `COUNTRY`) VALUES (%s, %s)"
+                    cursor.execute(sql, (customerid, country))
+                    connection.commit()
+                    if self.stop_event.is_set():
+                        break
+
+            connection.close()
+
+        customer_consumer.close()
+
+
+# class productConsumer(threading.Thread):
+
+def main():
+    tasks = [
+        customerConsumer()
+    ]
+
+    for t in tasks:
+        t.start()
+
+    time.sleep(10)
+
+    for task in tasks:
+        task.stop()
+
+    for task in tasks:
+        task.join()
+
 
 if __name__ == "__main__":
-    # print(sqlalchemy.__version__ )
-    # customer_consumer = KafkaConsumer('customer_in', bootstrap_servers='kafka1:9092')
-    # for msg in customer_consumer:
-    #     print(msg)
-    logging.basicConfig(filename='example.log',level=logging.DEBUG)
+    main()
 
-    customer_consumer = KafkaConsumer(bootstrap_servers='kafka1:9092', \
-                                      value_deserializer=lambda m: m.decode('utf-8'))
-    customer_consumer.subscribe(['customer_in'])
-
-    connection = pymysql.connect(host='mysql',
-                                 user='root',
-                                 password='233',
-                                 db='sales_data_pipeline')
-
-    with connection.cursor() as cursor:
-        for msg in customer_consumer:
-            customerid, country = msg.value.split(',')
-            logging.debug('%s %s', customerid, country)
-            sql = "INSERT INTO `customer` (`CUSTOMER_ID`, `COUNTRY`) VALUES (%s, %s)"
-            cursor.execute(sql, (customerid, country))
-            connection.commit()
-
-    connection.close()
+    # logging.basicConfig(filename='example.log',level=logging.DEBUG)
+    #
+    # customer_consumer = KafkaConsumer(bootstrap_servers='kafka1:9092', \
+    #                                   value_deserializer=lambda m: m.decode('utf-8'))
+    # customer_consumer.subscribe(['customer_in'])
+    #
+    # connection = pymysql.connect(host='mysql',
+    #                              user='root',
+    #                              password='233',
+    #                              db='sales_data_pipeline')
+    #
+    # with connection.cursor() as cursor:
+    #     for msg in customer_consumer:
+    #         customerid, country = msg.value.split(',')
+    #         logging.debug('%s %s', customerid, country)
+    #         sql = "INSERT INTO `customer` (`CUSTOMER_ID`, `COUNTRY`) VALUES (%s, %s)"
+    #         cursor.execute(sql, (customerid, country))
+    #         connection.commit()
+    #
+    # connection.close()
