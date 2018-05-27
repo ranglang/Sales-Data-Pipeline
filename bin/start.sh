b35838cb94db7504ea24930cfa5ade9965bc6dd7
Wenyi Xu <wenyixu@WenyideMacBook-Pro.local>
2018-05-27 11:04:55 -0500

    Added service airflow.

--- bin/start.sh
+++ bin/start.sh
@@ -7,5 +7,7 @@ sleep 10s
 sudo docker-compose build mysql-consumer
 sudo docker-compose up -d mysql-consumer
 sleep 10s
-sudo docker-compose build kafka-producer
-sudo docker-compose up -d kafka-producer
+sudo docker-compose build mock-producer
+sudo docker-compose up -d mock-producer
+sudo docker-compose build airflow
+sudo docker-compose up -d airflow
