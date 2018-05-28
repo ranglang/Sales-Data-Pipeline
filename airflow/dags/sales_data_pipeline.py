4000831a57b1afafe2c12fa43907e21b2519f33c
Wenyi Xu <wenyixu@WenyideMacBook-Pro.local>
2018-05-28 15:10:42 -0500

    Added spark test task.

--- airflow/dags/sales_data_pipeline.py
+++ airflow/dags/sales_data_pipeline.py
@@ -9,6 +9,9 @@ import os
 
 # Calculate the start date
 current_date = datetime.utcnow() - timedelta(days=5)
+# Set Spark path
+sparkSubmit = '/home/wenyixu/spark-2.3.0-bin-hadoop2.7/bin/spark-submit'
+srcDir = os.getcwd() + '/../src/'
 
 default_args = {
                 'owner': 'airflow',
@@ -25,11 +28,16 @@ default_args = {
                 'retry_delay': timedelta(minutes=5)
                 }
 
-sale_data_pipeline = DAG('weebly-v0', schedule_interval="@daily", catchup=False, default_args=default_args)
+# schedule_interval="@hourly"
+sale_data_pipeline = DAG('weebly-v0', schedule_interval=timedelta(minutes=5), catchup=False, default_args=default_args)
 
 task_dummy = BashOperator(task_id='task_dummy',
                           bash_command='touch os.path.dirname(__file__)/../../data/out/test.txt',
                           dag=sale_data_pipeline)
+
+task_spark_test = BashOperator(task_id='spark_test',
+                               bash_command=sparkSubmit + ' ' + srcDir + 'test.py',
+                               dag=sale_data_pipeline)
 
 # task_dummy = PythonOperator(task_id='get_customer_geo_dist',
 #                             python_callable=get_customer_geo_dist,
