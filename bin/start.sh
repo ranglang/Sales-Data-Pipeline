6f25471eaebc019bb6d96df364dfa114dc34471d
Wenyi Xu <wenyixu@WenyideMacBook-Pro.local>
2018-05-28 13:18:13 -0500

    Updated service name in start,sh.

--- bin/start.sh
+++ bin/start.sh
@@ -1,6 +1,7 @@
 sudo docker-compose up -d zoo1
 sudo docker-compose up -d kafka1
 sudo docker-compose up -d mysql
+sudo docker-compose up -d postgres
 sleep 10s
 . ./bin/kafka-setup.sh
 sleep 10s
@@ -9,5 +10,4 @@ sudo docker-compose up -d mysql-consumer
 sleep 10s
 sudo docker-compose build mock-producer
 sudo docker-compose up -d mock-producer
-sudo docker-compose build airflow
-sudo docker-compose up -d airflow
+sudo docker-compose up -d webserver
