d9565966868e1ca18efb6944219ae03eb8e89b45
Wenyi Xu <wenyixu@WenyideMacBook-Pro.local>
2018-05-28 15:10:53 -0500

    Created test.py for spoark test task.

---
+++ airflow/src/test.py
@@ -0,0 +1,12 @@
+from pyspark import SparkConf, SparkContext
+
+def f0(x):
+    with open("test.txt", "a+") as f:
+        f.write(str(x))
+
+conf = (SparkConf()
+         .setMaster("local")
+         .setAppName("My app"))
+sc = SparkContext(conf = conf)
+
+sc.parallelize([1, 2, 3, 4]).foreach(f0)
