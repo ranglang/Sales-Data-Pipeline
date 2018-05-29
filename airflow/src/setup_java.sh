53ed86539edf9508a3e59b3998057880bd819d5e
Wenyi Xu <wenyixu@WenyideMacBook-Pro.local>
2018-05-29 14:48:52 -0500

    Removed sudo.

--- airflow/src/setup_java.sh
+++ airflow/src/setup_java.sh
@@ -1,3 +1,7 @@
-sudo apt-get update
-sudo apt-get install default-jre
-sudo apt-get install default-jdk
+# sudo apt-get update
+# sudo apt-get install default-jre
+# sudo apt-get install default-jdk
+
+apt-get update
+apt-get install default-jre
+apt-get install default-jdk
