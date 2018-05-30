267f606b38193b718ae4e29a07c651b690965c97
Wenyi Xu <wenyixu@WenyideMacBook-Pro.local>
2018-05-30 12:04:13 -0500

    Updated spark logic.

--- airflow/src/test.py
+++ airflow/src/test.py
@@ -3,30 +3,27 @@ import pymysql
 import os
 
 
-def send_record_to_db(table_name, records):
+def processRecord(record):
     connection = pymysql.connect(host='mysql',
                                  user='root',
                                  password='233',
-                                 db='data_pipeline')
+                                 db='sales_data_pipeline')
 
     with connection.cursor() as cursor:
-        for record in records:
-            sql = "INSERT INTO `test` (`NUMBER`) VALUES (%s)"
-            cursor.execute(sql, record)
-            connection.commit()
-
+        sql = "INSERT INTO `test` (`CUSTOMER_ID`, `NAME`) VALUES (%s, %s)"
+        cursor.execute(sql, record)
+        connection.commit()
     connection.close()
 
 
-def send_to_db(rdd, table_name):
-    rdd.foreachPartition(lambda records: send_record_to_db(table_name, records))
-
-
-path = os.getcwd()
 
 conf = (SparkConf()
          .setMaster("local")
          .setAppName("My app"))
 sc = SparkContext(conf = conf)
 
-sc.parallelize([1, 2, 3, 4]).foreachRDD(lambda rdd: send_to_db(rdd, 'test'))
+data =['103', 'tester_1']
+
+rdd = sc.parallelize(data)
+
+rdd.foreach(processRecord)
