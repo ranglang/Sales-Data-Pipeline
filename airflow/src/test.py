91cf18f9df7aaea0de41df5ab54d7b1e43a360da
Wenyi Xu <wenyixu@WenyideMacBook-Pro.local>
2018-05-30 11:46:43 -0500

    Send data to MySQQL.

--- airflow/src/test.py
+++ airflow/src/test.py
@@ -1,9 +1,26 @@
 from pyspark import SparkConf, SparkContext
+import pymysql
 import os
 
-def f0(x):
-    with open(path + "/test.txt", "a+") as f:
-        f.write(str(x))
+
+def send_record_to_db(table_name, records):
+    connection = pymysql.connect(host='mysql',
+                                 user='root',
+                                 password='233',
+                                 db='data_pipeline')
+
+    with connection.cursor() as cursor:
+        for record in records:
+            sql = "INSERT INTO `test` (`NUMBER`) VALUES (%s)"
+            cursor.execute(sql, record)
+            connection.commit()
+
+    connection.close()
+
+
+def send_to_db(rdd, table_name):
+    rdd.foreachPartition(lambda records: send_record_to_db(table_name, records))
+
 
 path = os.getcwd()
 
@@ -12,4 +29,4 @@ conf = (SparkConf()
          .setAppName("My app"))
 sc = SparkContext(conf = conf)
 
-sc.parallelize([1, 2, 3, 4]).foreach(f0)
+sc.parallelize([1, 2, 3, 4]).foreachRDD(lambda rdd: send_to_db(rdd, 'test'))
